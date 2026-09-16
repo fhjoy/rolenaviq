40b57ad087b24d933577bb6b466727eee084bf80
Faisal Hossain <82816011+fhjoy@users.noreply.github.com>
2026-09-16 13:45:21 +0200

    Add public demo account configuration

---
+++ server/src/config/demo.ts
@@ -0,0 +1,4 @@
+export const DEMO_EMAIL = "demo@rolenaviq.app";
+export const DEMO_PASSWORD = "RoleNaviqDemo2026!";
+export const DEMO_FIRST_NAME = "Demo";
+export const DEMO_LAST_NAME = "Recruiter";
