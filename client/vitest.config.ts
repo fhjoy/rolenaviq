a74adb15aa0d659e2fc984f1c93516db696c9c5a
Md Faisal Hossain <faisalh.iu@gmail.com>
2026-09-21 12:30:48 +0200

    warning fix

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,6 +1,6 @@
 import { mergeConfig, defineConfig } from "vitest/config";
 
-import viteConfig from "./vite.config";
+import viteConfig from "./vite.config.ts";
 
 export default mergeConfig(
   viteConfig,
